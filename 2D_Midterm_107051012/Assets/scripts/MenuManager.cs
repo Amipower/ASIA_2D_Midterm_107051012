6c5dc4c8486eb0545320f5e36beced481cb9aecb
Amipower <71679909+Amipower@users.noreply.github.com>
2020-11-15 11:45:55 +0800

    編輯器溝通語法

--- 2D_Midterm_107051012/Assets/scripts/MenuManager.cs
+++ 2D_Midterm_107051012/Assets/scripts/MenuManager.cs
@@ -1,18 +1,21 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //註解:說明
+    //按鈕如何跟程式溝通
+    //需要:方法(函式`功能)-一堆有功能的程式，演算法
+    //語法:
+    //類型 方法名稱 () {}
+    //void 無類型
+    void StartGame()
     {
-        
+
+    }
+    void QuitGame()
+    {
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
