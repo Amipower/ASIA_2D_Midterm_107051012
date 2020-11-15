23fac800616dbf9de5fc298cda42a9bb02e05ee3
Amipower <71679909+Amipower@users.noreply.github.com>
2020-11-15 16:39:15 +0800

    觸發

--- 2D_Midterm_107051012/Assets/scripts/MenuManager.cs
+++ 2D_Midterm_107051012/Assets/scripts/MenuManager.cs
@@ -1,21 +1,28 @@
-﻿
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class NewBehaviourScript : MonoBehaviour
+public class MenuManager : MonoBehaviour
 {
     //註解:說明
     //按鈕如何跟程式溝通
     //需要:方法(函式`功能)-一堆有功能的程式，演算法
     //語法:
-    //類型 方法名稱 () {}
+    //修飾詞 類型 方法名稱(){}
+    //私人 private(預設) - 不可以被按鈕呼叫
+    //公開public - 可以被按鈕呼叫
     //void 無類型
-    void StartGame()
+    public void StartGame()
     {
+        print("開始遊戲"); //輸出(訊息)-文字要加雙引號，儀表板console
 
+        SceneManager.LoadScene("關卡1");  //場景管理，載入場景("名稱")
+        //SceneManager.LoadScene(1);      //場景管理，載入場景(編號)
     }
-    void QuitGame()
-    {
 
+    public void QuitGame()
+    {
+        print("離開遊戲");
+        Application.Quit();
     }
 
 }
---
+++ 2D_Midterm_107051012/Assets/scripts/player.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class player: MonoBehaviour
+{
+    //欄位:儲存資料
+    //語法:
+    //修飾詞 類型 名稱 (指定 值);
+    //None空值:沒東西-程式錯誤
+    public GameObject final;
+
+    //碰撞偵測條件
+    //1.兩個物件必須要有碰撞器Collider2D
+    //2.兩個至少必須要有一個剛體Rigidbody2D
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("碰到傳送門");
+    }
+    
+}
